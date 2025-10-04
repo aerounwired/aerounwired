8b2fd99a981d4ec6d3dc9b315c9dcb870261fb71
Arundas-s-g <arundas2005sg@gmail.com>
2025-10-04 23:00:39 +0530

    Update achievements timeline with image integration and CSS fitting

--- achievements.js
+++ achievements.js
@@ -1,9 +1,11 @@
 let achievedata = [
-  {name:"ADDC", about:"Unique Technology Award 2nd position Overall - AIR 26th", year:"2025"},
-  {name:"DDC",  about:"Unique Technology Award 2nd position Overall - AIR 26th", year:"2025"},
-  {name:"ADDC", about:"Unique Technology Award 2nd position Overall - AIR 26th", year:"2025"},
-  {name:"DDC",  about:"Unique Technology Award 2nd position Overall - AIR 26th", year:"2025"},
-  {name:"ADDC", about:"Unique Technology Award 2nd position Overall - AIR 26th", year:"2025"}
+  {name: "NITK", about: "AIR-5th in Wright Flight competition conducted by NITK", year: "2025", image: "gallery/NITK.jpg"},
+  {name: "ADDC", about: "Unique Technology Award 2nd position Overall - AIR 26th", year: "2025", image: "gallery/ADDC_25.jpg"},
+  {name: "DDC", about: "CFD rank-8th position Overall AIR-15th", year: "2025", image: "gallery/DDC_25.jpg"},
+  {name: "ADDC", about: "AIR-1 in best design report", year: "2023", image: "gallery/ADDC_23.jpg"},
+  {name: "ADC", about: "Earned Top Honors at Boeing-IIT National Aeromodelling Competition", year: "2019", image: "gallery/ADC_2019.jpg"},
+  {name: "AERO DESIGN", about: "Awarded 2nd position for Best Presentation Overall AIR-4th in Aero Design challenge", year: "2017", image: "gallery/Aero_design.jpg"},
+  {name: "ADDC", about: "AIR-1 in best design report", year: "2023", image: "gallery/ADDC_23.jpg"}
 ];
 
 let achievehtml = achievedata.map((data, index) => {
@@ -11,35 +13,38 @@ let achievehtml = achievedata.map((data, index) => {
     return `
       <div class="container left-container">
         <div class="text-box">
-        <div class="size">
-          <h2>${data.name}</h2>
-          <small>${data.year}</small>
-        </div>
+          <div class="size">
+            <h2>${data.name}</h2>
+            <small>${data.year}</small>
+          </div>
           <p>${data.about}</p>
         </div>
-        <div class="box box-left"></div>
+        <div class="box box-left">
+          <img src="${data.image}" alt="${data.about}" />
+        </div>
         <span class="left-container-arrow"></span>
       </div>`;
   } else {
     return `
       <div class="container right-container">
         <div class="text-box text-box-right">
-         <div class="size">
-          <h2>${data.name}</h2>
-          <small>${data.year}</small>
-        </div>
+          <div class="size">
+            <h2>${data.name}</h2>
+            <small>${data.year}</small>
+          </div>
           <p>${data.about}</p>
         </div>
-        <div class="box right-box"></div>
+        <div class="box right-box">
+          <img src="${data.image}" alt="${data.about}" />
+        </div>
         <span class="right-container-arrow"></span>
       </div>`;
   }
-}).join(""); // join avoids stray commas
+}).join("");
 
 document.querySelector(".timeline").innerHTML = achievehtml;
 
-
-
+// ... (rest of your JS remains the same)
 
 
 
